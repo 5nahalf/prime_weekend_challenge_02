13569aa9e4306ea9420669ca4423e5be908904bb
5nahalf <5nahalf@comcast.net>
2015-04-24 18:43:17 -0500

    made it kinda work

--- script.js
+++ script.js
@@ -2,36 +2,100 @@
 $(document).ready(function(){
 	$("#btn2").on("click", function(){
 		shuffleArray(people);
-		console.log(people);
-
+		team2 = people.splice(0, 10);
 
 	});
 	$("#btn3").on("click", function(){
+		shuffleArray(people);
+		team2 = people.splice(0, 7);
+		team3 = people.splice(0, 6);
+	
+
 
 	});
 	$("#btn4").on("click", function(){
+		shuffleArray(people);
+		team2 = people.splice(0, 5);
+		team3 = people.splice(0, 5);
+		team4 = people.splice(0, 5);
 
 	});
 	$("#btn5").on("click", function(){
+		shuffleArray(people);
+		team2 = people.splice(0, 4);
+		team3 = people.splice(0, 4);
+		team4 = people.splice(0, 4);
+		team5 = people.splice(0, 4);
 
 	});
 	$("#btn6").on("click", function(){
+		shuffleArray(people);
+		team2 = people.splice(0, 4);
+		team3 = people.splice(0, 3);
+		team4 = people.splice(0, 3);
+		team5 = people.splice(0, 3);
+		team6 = people.splice(0, 3);
 
 	});
 	$("#btn7").on("click", function(){
+		shuffleArray(people);
+		team2 = people.splice(0, 3);
+		team3 = people.splice(0, 3);
+		team4 = people.splice(0, 3);
+		team5 = people.splice(0, 3);
+		team6 = people.splice(0, 3);
+		team7 = people.splice(0, 3);
 
 	});
 	$("#btn8").on("click", function(){
+		shuffleArray(people);
+		team2 = people.splice(0, 3);
+		team3 = people.splice(0, 3);
+		team4 = people.splice(0, 3);
+		team5 = people.splice(0, 2);
+		team6 = people.splice(0, 2);
+		team7 = people.splice(0, 2);
+		team8 = people.splice(0, 2);
 
 	});
 	$("#btn9").on("click", function(){
+		shuffleArray(people);
+		team2 = people.splice(0, 3);
+		team3 = people.splice(0, 2);
+		team4 = people.splice(0, 2);
+		team5 = people.splice(0, 2);
+		team6 = people.splice(0, 2);
+		team7 = people.splice(0, 2);
+		team8 = people.splice(0, 2);
+		team9 = people.splice(0, 2);
 
 	});
 	$("#btn10").on("click", function(){
+		shuffleArray(people);
+		team2 = people.splice(0, 2);
+		team3 = people.splice(0, 2);
+		team4 = people.splice(0, 2);
+		team5 = people.splice(0, 2);
+		team6 = people.splice(0, 2);
+		team7 = people.splice(0, 2);
+		team8 = people.splice(0, 2);
+		team9 = people.splice(0, 2);
+		team10 = people.splice(0, 2);
 
 	});
 	$("#refresh").click(function(){
 		$("p").empty();
+		$("p").append("<div id='team1'>" + people + "</div>");
+		$("p").append("<div id='team2'>" + team2 + "</div>");
+		$("p").append("<div id='team3'>" + team3 + "</div>");
+		$("p").append("<div id='team4'>" + team4 + "</div>");
+		$("p").append("<div id='team5'>" + team5 + "</div>");
+		$("p").append("<div id='team6'>" + team6 + "</div>");
+		$("p").append("<div id='team7'>" + team7 + "</div>");
+		$("p").append("<div id='team8'>" + team8 + "</div>");
+		$("p").append("<div id='team9'>" + team9 + "</div>");
+		$("p").append("<div id='team10'>" + team10 + "</div>");
+
 	});
 
 
@@ -39,8 +103,8 @@ $(document).ready(function(){
 });
 
 
-var people = ["Erik", "Aaron", "Alicia", "Brian", "Casie", "Clare", "Cody", "Jeanne", "Kaitlin", "Kelly", "Luke", "Mary", "Michael", "Michelle", "Rom", "Steve", "Terry", "Tracy", "Vince"];
-
+var people = ["Erik", "Aaron", "Alicia", "Casie", "Clare", "Cody", "Jeanne", "Kaitlin", "Kelly", "Luke", "Mary", "Michael", "Michelle", "Rom", "Steve", "Terry", "Tracy", "Vince", "Brian", "Chelsea"];
+console.log(people.length)
 
 var team2 = [];
 var team3 = [];
@@ -65,6 +129,4 @@ function shuffleArray(array) {
     }
     return array;
 };
-// function randomNumber(min, max) {
-// 	return Math.floor(Math.random() * (1 + max - min) + min);
-// }
+
