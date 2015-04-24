5f9d086f1d139e340ba27b79d7ef5579bc26c11c
5nahalf <5nahalf@comcast.net>
2015-04-24 16:38:35 -0500

    add first files

---
+++ script.js
@@ -0,0 +1,46 @@
+
+$(document).ready(function(){
+	$("#btn2").on("click", function(){
+
+	});
+	$("#btn3").on("click", function(){
+
+	});
+	$("#btn4").on("click", function(){
+
+	});
+	$("#btn5").on("click", function(){
+
+	});
+	$("#btn6").on("click", function(){
+
+	});
+	$("#btn7").on("click", function(){
+
+	});
+	$("#btn8").on("click", function(){
+
+	});
+	$("#btn9").on("click", function(){
+
+	});
+	$("#btn10").on("click", function(){
+
+	});
+	$("#refresh").click(function(){
+		$("p").empty();
+	});
+
+
+	
+});
+
+
+var people = ["Erik", "Aaron", "Alicia", "Brian", "Casie", "Clare", "Cody", "Jeanne", "Kaitlin", "Kelly", "Luke", "Mary", "Michael", "Michelle", "Rom", "Steve", "Terry", "Tracy", "Vince"];
+
+
+
+
+function randomNumber(min, max) {
+	return Math.floor(Math.random() * (1 + max - min) + min);
+}
